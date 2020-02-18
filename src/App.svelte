19e8fd35b200cbc9f55dab7d843480a65024f7cc
Thomas Nelson <thnlsn@gmail.com>
2020-02-17 16:30:51 -0800

    binding working

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,22 @@
   let name = "Thomas";
   let age = 22;
 
-  function incrementAge(number) {
-    return age + number;
+  // rerun this whenever name changes
+  $: uppercaseName = name.toUpperCase(); // whenever the application reloads it will always make name uppercase even if it changes
+
+  $: console.log();
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "Thomas Nelson";
+  }
+
+  function inputChange(e) {
+    const enteredValue = event.target.value; // when a value is entered, it triggers and event, and this function runs, creating a variable of the current value of the input and setting name to that
+    name = enteredValue;
   }
 </script>
 
@@ -13,5 +27,9 @@
   }
 </style>
 
-<h1>Hello {name}, I am {age} years old!</h1>
-<button on:click={incrementAge} />
+<h1>My name is {uppercaseName}, I am {age} years old!</h1>
+<button type="submit" on:click={incrementAge}>Increment</button>
+<!-- <button type="submit" on:click={changeName}>Fullname</button> -->
+<!-- <input type="text" on:input={inputChange} value={name} id="" /> -->
+<input type="text" bind:value={name} />
+<!-- forceful two-way connection {value and name will always be the same} -->
